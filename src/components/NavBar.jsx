dd7859d4435372e104d2cdf713cc5c5f67ca840e
Debajyoti Pandit <codeninja0812@gmail.com>
2024-02-25 21:45:08 +0530

    Nav Bar completed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Link} from "react-router-dom"
-import { RiArrowDropDownLine } from "react-icons/ri";
+// import { RiArrowDropDownLine } from "react-icons/ri";
+import { IoIosArrowDropdownCircle } from "react-icons/io";
+import { FaUserCircle } from "react-icons/fa";
 
 
 const NavItems = [
@@ -18,6 +20,29 @@ const NavItems = [
         id:3,
         name:"Contact",
         link:'/contact'
+    },
+
+]
+const DropdownItems = [
+    {
+        id:1,
+        name:"Beverages",
+        link:'/beverages'
+    },
+    {
+        id:2,
+        name:"Food Items",
+        link:'/fooditems'
+    },
+    {
+        id:3,
+        name:"Deserts",
+        link:'/deserts'
+    },
+    {
+        id:4,
+        name:"Snacks",
+        link:'/snacks'
     },
 
 ]
@@ -47,6 +72,63 @@ const NavBar = () => {
                             
                         )
                         }
+
+                        {/* Dropdown cover */}
+                        <li className='cursor-pointer group relative'>
+                            <Link to="/categories" className='inline-block hover:text-primary text-xl font-semibold'>
+                                <div className='flex items-center gap-[2px] py-2'>
+                                    Categories
+                                    <span>
+                                        <IoIosArrowDropdownCircle 
+                                        className='group-hover:rotate-180 duration-300'
+                                        />
+                                    </span>
+                                    
+                                </div>
+                            
+                            
+                            </Link>
+                            {/* Dropdown implementation */}
+                            <div className='absolute z-[9999] hidden group-hover:block w-[200px] bg-white text-black shadow-md p-2'>
+                                <ul>
+                                    {
+                                        DropdownItems.map(({id , name , link})=>{
+                                            return(
+                                                <li key={id}>
+                                                    <Link to={link}
+                                                    className='text-xl inline-block w-full rounded-md p-2 hover:bg-primary/20'
+                                                    
+                                                    >
+                                                        {name} 
+                                                    </Link>
+                                                </li>
+                                            )
+                                        })
+                                    }
+
+                                </ul>
+                            </div>
+
+                        </li>
+                        {/* Sign Up button section */}
+                        <li>
+                            <button className='flex justify-center items-center gap-2 bg-secondary text-xl h-[40px] text-white px-5 py-2 hover:scale-105 duration-300 rounded-lg'>
+                                <FaUserCircle />
+                                Sign Up
+
+                            </button>
+                        </li>
+
+                        {/* Sign in button section */}
+                        <li>
+                            <button className='flex justify-center items-center gap-2 bg-pink-500 text-xl h-[40px] text-white px-5 py-2 hover:scale-105 duration-300 rounded-lg'>
+                                <FaUserCircle />
+                                Sign In
+
+                            </button>
+                        </li>
+
+                        
                     </ul>
                 </div>
 
